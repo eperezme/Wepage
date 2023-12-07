a3c188b06fde08bfada494e5ef42a641f56cf468
Dudu <eduardperezmendez@gmail.com>
2023-12-07 17:04:53 +0100

    Delete unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { compression } from 'vite-plugin-compression2';
+import { compression } from 'vite-plugin-compression2';
 import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // compression({ algorithm: 'brotliCompress'}),
+    compression({ algorithm: 'brotliCompress'}),
     imagetools()],
   server: {
     port: 3000
