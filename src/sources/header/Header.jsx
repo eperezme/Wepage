f5a060ecf00648129f1d4a971adf64887e1a7524
Dudu <eduardperezmendez@gmail.com>
2023-12-07 16:37:25 +0100

    Fix image import and add support for webp format

--- src/sources/header/Header.jsx
+++ src/sources/header/Header.jsx
@@ -3,7 +3,8 @@ import Typewriter from '../../functions/typewrite.jsx'
 
 
 // IMAGES
-import IMG1 from '../../assets/principal.png'
+import IMG1 from '../../assets/principal.png??w=300;400;500;600;700;800;900;1000;1200;1500;1800;2000&format=webp&as=srcset';
+import IMG1_fall from '../../assets/principal.png'
 
 
 
@@ -18,7 +19,9 @@ const Header = () => {
       <div className="color-header home-campaign-productivity js-section px-3 pb-0 pt-5">
         <div className="container-xl position-relative">
           {/* [x] Fix image */}
-          <img alt="" aria-hidden="true" width="2236" height="1630" className="image-fade top-0 position-absolute height-auto events-none d-none d-sm-block" style={{width: "min(1118px, max(100vh, 100vw))", left: "40%", overflowX: "hidden", overflowY: "visible"}} src={IMG1}/>
+          <picture>
+            <source srcSet={IMG1} type='image/webp'/>
+            <img alt="" aria-hidden="true" width="2236" height="1630" className="image-fade top-0 position-absolute height-auto events-none d-none d-sm-block" style={{width: "min(1118px, max(100vh, 100vw))", left: "40%", overflowX: "hidden", overflowY: "visible"}} src={IMG1_fall}/></picture>
         </div>
         <div className="overflow-hidden color-header home-campaign-hero">
           <div className="position-relative pt-3 pt-sm-8 pt-lg-12 container-xl js-build-in-trigger build-in-animate">
