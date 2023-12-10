d7b4424c2fcaa5c2e2b4959dc1f2fc34424275c3
Dudu <eduardperezmendez@gmail.com>
2023-12-10 13:48:33 +0100

    Add Intersection Observer to animate elements

--- src/functions/observer.jsx
+++ src/functions/observer.jsx
@@ -1,33 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-// There is only one class that triggers the animation 
-// ==== .build-in-animate ====
-// This class is added to the element that you want to animate
-// There are two types of triggers
-// ==== .js-build-in-trigger ==== 
-// ==== .js-build-in-item ==== This are individual elements that are animated
+
 
 const IntersectionObserverComponent = ({ children }) => {
+  const observerRef = useRef(null);
+
   useEffect(() => {
     const observerCallback = (entries) => {
       entries.forEach((entry) => {
-        console.log(entry);
+        // Check if the bottom of the element is above the viewport
         if (entry.isIntersecting) {
           entry.target.classList.add('build-in-animate');
-        } else {
+        } else if (entry.boundingClientRect.top > 0) {
           entry.target.classList.remove('build-in-animate');
         }
       });
     };
 
-    const observer = new IntersectionObserver(observerCallback, { threshold: 0.5 });
+    // That should threshold the js-build-in-trigger element
+    observerRef.current = new IntersectionObserver(observerCallback, { threshold: 0.5 });
 
     const animateElements = document.querySelectorAll('.js-build-in-item');
-    animateElements.forEach((el) => observer.observe(el));
+    animateElements.forEach((el) => observerRef.current.observe(el));
 
     // Clean up the observer when the component unmounts
     return () => {
-      animateElements.forEach((el) => observer.unobserve(el));
+      animateElements.forEach((el) => observerRef.current.unobserve(el));
     };
   }, []); // Empty dependency array to ensure useEffect runs only once when the component mounts
 
