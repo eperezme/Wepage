1d8cc6a14cdb04a7be2ac9ce2058cb4324e8361b
Dudu <eduardperezmendez@gmail.com>
2023-12-10 14:38:39 +0100

    Refactor Intersection Observer logic to handle multiple trigger elements

--- src/functions/observer.jsx
+++ src/functions/observer.jsx
@@ -1,45 +1,43 @@
 import React, { useEffect, useRef } from 'react';
 
 const IntersectionObserverComponent = ({ children }) => {
-  const triggerObserverRef = useRef(null);
-
   useEffect(() => {
-    // Callback function for the Intersection Observer
-    const triggerObserverCallback = (entries) => {
-      entries.forEach((entry) => {
-        const triggerElement = entry.target.closest('.js-build-in-trigger');
+    // Find all elements with the class 'js-build-in-trigger'
+    const triggerElements = document.querySelectorAll('.js-build-in-trigger');
 
-        if (entry.isIntersecting) {
-          // If the trigger element is in the viewport, add animation class
-          if (triggerElement) {
-            triggerElement.classList.add('build-in-animate');
-            const itemsInTrigger = triggerElement.querySelectorAll('.js-build-in-item');
-            itemsInTrigger.forEach((item) => item.classList.add('build-in-animate'));
-          }
-        } else if (entry.boundingClientRect.top > 0) {
-          // If the trigger element is scrolled above the viewport, remove animation class
-          if (triggerElement) {
-            triggerElement.classList.remove('build-in-animate');
-            const itemsInTrigger = triggerElement.querySelectorAll('.js-build-in-item');
-            itemsInTrigger.forEach((item) => item.classList.remove('build-in-animate'));
-          }
-        }
-      });
-    };
+    // Loop through each trigger element and create a separate Intersection Observer for each
+    triggerElements.forEach((triggerElement) => {
+      const triggerObserver = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              // If the trigger element is at least half in the viewport, add animation class
+              triggerElement.classList.add('build-in-animate');
+              const itemsInTrigger = triggerElement.querySelectorAll('.js-build-in-item');
+              itemsInTrigger.forEach((item) => item.classList.add('build-in-animate'));
+            } else if (entry.boundingClientRect.top > window.innerHeight && entry.isIntersecting === false) {
+              // If the trigger element is scrolled above the viewport, remove animation class
+              triggerElement.classList.remove('build-in-animate');
+              const itemsInTrigger = triggerElement.querySelectorAll('.js-build-in-item');
+              itemsInTrigger.forEach((item) => item.classList.remove('build-in-animate'));
+            }
+          });
+        },
+        
+      );
 
-    // Create the Intersection Observer for the trigger element
-    triggerObserverRef.current = new IntersectionObserver(triggerObserverCallback, { threshold: 0.5 });
+      // Find all elements with the class 'js-build-in-item' inside the current trigger element
+      const animateElements = triggerElement.querySelectorAll('.js-build-in-item');
 
-    // Find all elements with the class 'js-build-in-item'
-    const animateElements = document.querySelectorAll('.js-build-in-item');
+      // Observe each 'js-build-in-item' element using the triggerObserver
+      animateElements.forEach((el) => triggerObserver.observe(el));
 
-    // Observe each 'js-build-in-item' element using the triggerObserver
-    animateElements.forEach((el) => triggerObserverRef.current.observe(el));
-
-    // Clean up the observer when the component unmounts
-    return () => {
-      animateElements.forEach((el) => triggerObserverRef.current.unobserve(el));
-    };
+      // Clean up the observer when the trigger element is removed from the DOM
+      return () => {
+        animateElements.forEach((el) => triggerObserver.unobserve(el));
+        triggerObserver.disconnect();
+      };
+    });
   }, []); // Empty dependency array to ensure useEffect runs only once when the component mounts
 
   return <div>{children}</div>;
