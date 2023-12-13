f4dbcc75b628f7c28a1cc703bf6f4b107c0cd5f8
Dudu <eduardperezmendez@gmail.com>
2023-12-13 19:48:35 +0100

    Update index.css and Experience.jsx, and Skills.jsx

--- src/sources/skills/Skills.jsx
+++ src/sources/skills/Skills.jsx
@@ -25,8 +25,8 @@ function Skills() {
   <div className='d-flex js-build-in-trigger position-relative'>
     <div className='col-1 d-flex flex-column flex-items-center'>
       <div className='mb-4 position-relative home-campaign-glowing-icon js-build-in-item build-in-scale-fade' style={{transitionDelay: "0ms"}}>
-      <svg aria-hidden="true" height="24" viewBox="0 0 24 24" version="1.1" width="24" data-view-component="true" className="octicon octicon-briefcase">
-        <path d="M7.5 1.75C7.5.784 8.284 0 9.25 0h5.5c.966 0 1.75.784 1.75 1.75V4h4.75c.966 0 1.75.784 1.75 1.75v14.5A1.75 1.75 0 0 1 21.25 22H2.75A1.75 1.75 0 0 1 1 20.25V5.75C1 4.784 1.784 4 2.75 4H7.5Zm-5 10.24v8.26c0 .138.112.25.25.25h18.5a.25.25 0 0 0 .25-.25v-8.26A4.235 4.235 0 0 1 18.75 13H5.25a4.235 4.235 0 0 1-2.75-1.01Zm19-3.24v-3a.25.25 0 0 0-.25-.25H2.75a.25.25 0 0 0-.25.25v3a2.75 2.75 0 0 0 2.75 2.75h13.5a2.75 2.75 0 0 0 2.75-2.75Zm-6.5-7a.25.25 0 0 0-.25-.25h-5.5a.25.25 0 0 0-.25.25V4h6Z"></path>
+      <svg aria-hidden="true" height="24" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" className="octicon octicon-briefcase">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"/>
       </svg>
       <span className='position-absolute left-0 top-0 height-full width-full home-campaign-glowing-icon-glow circle z-n1' style={{backgroundColor: "var(--mktg-accent-primary)", filter: "blur(18px)"}}></span>
       </div>
